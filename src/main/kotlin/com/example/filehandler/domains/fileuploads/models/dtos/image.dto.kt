29c8ac4966e0e902b007cad4175331cceabe7b93
Sayem Hossain <mail@sayem.dev>
2023-12-20 23:45:03 +0600

    Added id

--- src/main/kotlin/com/example/filehandler/domains/fileuploads/models/dtos/image.dto.kt
+++ src/main/kotlin/com/example/filehandler/domains/fileuploads/models/dtos/image.dto.kt
@@ -5,14 +5,17 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.Instant
 
 data class ImageDto(
+    @JsonProperty("id")
+    val id: Long,
+
     @JsonProperty("created_at")
     val createdAt: Instant,
 
     @JsonProperty("updated_at")
     val updatedAt: Instant,
 
-    @JsonProperty("uploaded_by")
-    val uploadedBy: String,
+    @JsonProperty("created_by")
+    val createdBy: String,
 
     @JsonProperty("image_url")
     val imageUrl: String,
@@ -22,9 +25,10 @@ data class ImageDto(
 )
 
 fun UploadedImage.toDto(baseUrl: String) = ImageDto(
+    id = this.id,
     createdAt = this.createdAt,
     updatedAt = this.updatedAt,
-    uploadedBy = this.createdBy,
+    createdBy = this.createdBy,
     imageUrl = "$baseUrl/${this.image.fileUrl}",
     thumbUrl = "$baseUrl/${this.thumb.fileUrl}"
 )
